471bf536f70e0c0e2e8c471e5a5bc3f3be187182
Elias Finoli <elle_finoli@hotmail.com>
2018-07-01 21:20:02 +0200

    QEConstantBuffer class done

--- QuoteEngine/QEConstantBuffer.cpp
+++ QuoteEngine/QEConstantBuffer.cpp
@@ -1,12 +1,41 @@
 #include "QEConstantBuffer.h"
+#include "QERenderingModule.h"
 
+QuoteEngine::QEConstantBuffer::QEConstantBuffer(size_t bufferSize, void * initialData) 
+	: m_BufferSize(bufferSize)
+{
+	//Reset the ComPtr
+	m_Buffer.Reset();
 
+	m_Data = new BYTE[bufferSize];
 
-QEConstantBuffer::QEConstantBuffer()
+	//Create the buffer.
+	create();
+}
+
+QuoteEngine::QEConstantBuffer::~QEConstantBuffer()
 {
 }
 
-
-QEConstantBuffer::~QEConstantBuffer()
+ID3D11Buffer * QuoteEngine::QEConstantBuffer::get()
 {
+	return m_Buffer.Get();
+}
+
+HRESULT QuoteEngine::QEConstantBuffer::create()
+{
+	D3D11_BUFFER_DESC cbDesc = {};
+	cbDesc.ByteWidth = m_BufferSize;
+	cbDesc.Usage = D3D11_USAGE_DYNAMIC;
+	cbDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cbDesc.MiscFlags = 0;
+	cbDesc.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA InitData;
+	InitData.pSysMem = m_Data;
+	InitData.SysMemPitch = 0;
+	InitData.SysMemSlicePitch = 0;
+
+	return QERenderingModule::gDevice->CreateBuffer(&cbDesc, &InitData, m_Buffer.ReleaseAndGetAddressOf());
 }
--- QuoteEngine/QEConstantBuffer.h
+++ QuoteEngine/QEConstantBuffer.h
@@ -1,8 +1,29 @@
 #pragma once
-class QEConstantBuffer
-{
-public:
-	QEConstantBuffer();
-	~QEConstantBuffer();
-};
+#include <d3d11.h>
+#include <wrl.h>
 
+namespace QuoteEngine
+{
+	class QEConstantBuffer
+	{
+	public:
+		QEConstantBuffer(size_t bufferSize, void* initialData);
+		~QEConstantBuffer();
+	
+		ID3D11Buffer* get();
+	private:
+		//The ID3D11Buffer pointer for this constant buffer
+		Microsoft::WRL::ComPtr<ID3D11Buffer> m_Buffer;
+		
+		//The size of the buffer.
+		const size_t m_BufferSize;
+
+		//The data of the buffer. This data will be updated by clients and when
+		//a client is about to bind the buffer, the actual buffer will be updated 
+		//with this data.
+		void* m_Data = nullptr;
+
+		//Creates the buffer
+		HRESULT create();
+	};
+}
