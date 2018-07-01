dbd25f0aaf8ce4657dc2e8787d1d81c839d4bb7a
Elias Finoli <elle_finoli@hotmail.com>
2018-07-01 20:52:38 +0200

    defined addConstantBuffers function

--- QuoteEngine/QEShader.cpp
+++ QuoteEngine/QEShader.cpp
@@ -2,11 +2,17 @@
 
 
 
-QEShader::QEShader()
+QuoteEngine::QEShader::QEShader()
 {
 }
 
 
-QEShader::~QEShader()
+QuoteEngine::QEShader::~QEShader()
 {
+}
+
+void QuoteEngine::QEShader::addConstantBuffers(std::vector<std::pair<unsigned int, QEConstantBuffer*>> buffers)
+{
+	for (auto pair : buffers)
+		m_ConstantBuffers.push_back(pair);
 }
