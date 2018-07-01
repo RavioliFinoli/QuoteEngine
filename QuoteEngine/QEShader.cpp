127bf808c7f262a56585451f211d24257ffca965
Elias Finoli <elle_finoli@hotmail.com>
2018-07-01 20:53:53 +0200

    defined addTextures function

--- QuoteEngine/QEShader.cpp
+++ QuoteEngine/QEShader.cpp
@@ -16,3 +16,9 @@ void QuoteEngine::QEShader::addConstantBuffers(std::vector<std::pair<unsigned in
 	for (auto pair : buffers)
 		m_ConstantBuffers.push_back(pair);
 }
+
+void QuoteEngine::QEShader::addTextures(std::vector<std::pair<unsigned int, QETexture*>> textures)
+{
+	for (auto pair : textures)
+		m_Textures.push_back(pair);
+}
