b1ad525f2f64a66c4568ace13646b6e77c5edb02
Elias Finoli <elle_finoli@hotmail.com>
2018-07-02 20:32:31 +0200

    Added vertex buffer member (ID3D11Buffer) to QEModel and implemented default construction in create()

--- QuoteEngine/QEModel.cpp
+++ QuoteEngine/QEModel.cpp
@@ -1,12 +1,56 @@
 #include "QEModel.h"
-
+#include "QERenderingModule.h"
 
 
 QEModel::QEModel()
 {
+	m_VertexBuffer.Reset(); //clean ComPtr
+
+	create();
 }
 
 
 QEModel::~QEModel()
 {
+}
+
+HRESULT QEModel::create()
+{
+	struct TriangleVertex
+	{
+		float x, y, z;
+		float r, g, b;
+	};
+
+	// Array of Structs (AoS)
+	TriangleVertex triangleVertices[3] =
+	{
+		0.0f, 0.5f, 0.0f,	//v0 pos
+		1.0f, 0.0f, 0.0f,	//v0 color
+
+		0.5f, -0.5f, 0.0f,	//v1
+		0.0f, 1.0f, 0.0f,	//v1 color
+
+		-0.5f, -0.5f, 0.0f, //v2
+		0.0f, 0.0f, 1.0f	//v2 color
+	};
+
+	// Describe the Vertex Buffer
+	D3D11_BUFFER_DESC bufferDesc;
+	memset(&bufferDesc, 0, sizeof(bufferDesc));
+	// what type of buffer will this be?
+	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	// what type of usage (press F1, read the docs)
+	bufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	// how big in bytes each element in the buffer is.
+	bufferDesc.ByteWidth = sizeof(triangleVertices);
+
+	// this struct is created just to set a pointer to the
+	// data containing the vertices.
+	D3D11_SUBRESOURCE_DATA data;
+	data.pSysMem = triangleVertices;
+
+	// create a Vertex Buffer
+	QERenderingModule::gDevice->CreateBuffer(&bufferDesc, &data, m_VertexBuffer.ReleaseAndGetAddressOf());
+	return E_NOTIMPL;
 }
--- QuoteEngine/QEModel.h
+++ QuoteEngine/QEModel.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <d3d11.h>
+#include <wrl.h> //ComPtr
+
 class QEModel
 {
 public:
-	QEModel();
+	QEModel(); //Default ctor for now, everything will be hardcoded for the time being
 	~QEModel();
+private:
+
+	Microsoft::WRL::ComPtr<ID3D11Buffer> m_VertexBuffer;
+
+	HRESULT create();
 };
 
